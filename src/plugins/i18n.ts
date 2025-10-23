97bee3816187917a785efed89850b856f7a1dae0
ZhuElly <34325707+Elly2018@users.noreply.github.com>
2025-10-24 03:27:09 +0800

    Update comments

--- src/plugins/i18n.ts
+++ src/plugins/i18n.ts
@@ -7,7 +7,20 @@ import { createI18n } from 'vue-i18n'
 import en from './../lan/en.json'
 import zh_TW from './../lan/zh_TW.json'
 
-export const i18nData = {
+export { createI18n as Create }
+
+/**
+ * Default vue-i18nconfig\
+ * It follows the format where link below describe\
+ * {@link https://vue-i18n.intlify.dev/api/general}\
+ * Import them simple use them
+ * @example
+ * // Use The Plugin
+ * import { createApp } from 'vue'
+ * const app = createApp(App)
+ * app.use(i18n)
+ */
+export const i18nDefaultData:any = {
     locale: 'en',
     globalInjection: true,
     fallbackFormat: 'en',
@@ -17,4 +30,8 @@ export const i18nData = {
     }
 }
 
-export const i18n = createI18n(i18nData)
+/**
+ * The language module which translate key into setup text string\
+ * This apply the default config
+ */
+export const i18n = createI18n(i18nDefaultData)
